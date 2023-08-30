f704055a507e560d938182a027af938149f243b6
suchangjie <1052207914@qq.com>
2023-08-30 23:08:00 +0800

    逐步实现redis缓存

---
+++ src/main/java/com/scj/foolRpcServer/cache/redis/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.scj.foolRpcServer.cache.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author suchangjie.NANKE
+ * @Title: RedisConfig
+ * @date 2023/8/30 22:54
+ * @description redis配置
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // String 类型 - 序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        // hash 类型 - 序列化
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        // 注入连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        return redisTemplate;
+    }
+
+}
+
---
+++ src/main/java/com/scj/foolRpcServer/cache/redis/RedisOpe.java
@@ -0,0 +1,71 @@
+package com.scj.foolRpcServer.cache.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author suchangjie.NANKE
+ * @Title: RedisTemplate
+ * @date 2023/8/29 23:01
+ * @description redis操作工具类
+ */
+@Component
+@Slf4j
+public class RedisOpe {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 缓存value
+     * 带超时时间
+     * @param key 键
+     * @param value 值
+     * @param time 过期时间
+     * @param timeUnit 时间单位
+     * @return 缓存是否成功
+     */
+    public boolean cacheValueWithExpireTime(String key, Object value, long time, TimeUnit timeUnit) {
+        try {
+            if (time > 0) {
+                // 如果有设置超时时间的话
+                redisTemplate.opsForValue().set(key, value, time, timeUnit);
+            } else {
+                redisTemplate.opsForValue().set(key, value);
+            }
+            return true;
+        } catch (Throwable e) {
+            log.error("缓存[" + key + "]失败, value[" + value + "] " + e.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * 缓存value
+     * 无超时时间
+     * @param key 键
+     * @param value 值
+     * @return 缓存是否成功
+     */
+    public boolean cacheValue(String key, Object value) {
+        return cacheValueWithExpireTime(key, value, 0, null);
+    }
+
+    /**
+     * 获取缓存
+     * @param key 键
+     * @return 值
+     */
+    public Object getValue(String key){
+        try {
+            return redisTemplate.opsForValue().get(key);
+        } catch (Throwable e){
+            log.error("获取缓存[{}]失败, error = {}", key, e.getMessage());
+        }
+        return null;
+    }
+}
