ee7677bfbb08ba5712180ad9e3e6c6e3cb8809ba
suchangjie <1052207914@qq.com>
2023-09-18 00:23:18 +0800

    nginx add

--- src/main/java/com/scj/foolRpcServer/register/FoolRegServer.java
+++ src/main/java/com/scj/foolRpcServer/register/FoolRegServer.java
@@ -14,7 +14,6 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.haproxy.HAProxyMessageDecoder;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
